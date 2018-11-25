59fd6e6d501aaf64099ffe65796737ee9333eed8
Siarhei Mahlysh <maglish@mail.ru>
2018-11-25 22:41:19 +0300

    Initial commit

---
+++ TeamCityTesting/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TeamCityTesting")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TeamCityTesting")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("41c63378-c745-46df-b21e-67937c842dfe")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ TeamCityTesting/UnitTest1.cs
@@ -0,0 +1,33 @@
+﻿namespace TeamCityTesting
+{
+    using OpenQA.Selenium;
+    using OpenQA.Selenium.Chrome;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class UnitTest1
+    {
+        private IWebDriver driver;
+
+        public UnitTest1()
+        {
+            this.driver = new ChromeDriver();
+            this.driver.Manage().Window.Maximize();
+            this.driver.Navigate().GoToUrl("https://www.jetbrains.com/");
+        }
+
+        [TestMethod]
+        public void JetBrainsTitleTest()
+            => Assert.IsTrue(this.driver.Title.Equals("JetBrains: Developer Tools for Professionals and Teams"));
+
+        [TestMethod]
+        public void JetBrainsLogoShouldBeDisplayedOnHomePage() 
+            => Assert.IsTrue(this.driver.FindElement(By.ClassName("header-logo")).Displayed);
+
+        [TestCleanup]
+        public void TestCleanUp()
+        {
+            this.driver.Quit();
+        }
+    }
+}
